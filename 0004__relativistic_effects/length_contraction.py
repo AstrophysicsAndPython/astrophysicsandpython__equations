6c11e7f0dedcf5e59493f61cda4b6194a3950c40
Noonuu <astrophysicsandpython@gmail.com>
2022-02-20 22:30:06 +0500

    major changes in relativistic_mass.py, minor changes in length_contraction.py.

--- 0004__relativistic_effects/length_contraction.py
+++ 0004__relativistic_effects/length_contraction.py
@@ -16,33 +16,34 @@ class LengthContraction:
         self.v, self.l0, self.lc = None, None, None
         self.c = 299792458 if self.unit == '/ms' else 1 if self.unit == 'rel' else -1
 
-    def beta(self) -> float:
-        return np.sqrt(1 - (self.v / self.c)**2)
-
     def __check(self):
-        if self.v == -1:
-            raise errors.VelocityError('Velocity of the object cannot be less than 0.')
-        elif np.logical_or(self.l0 == -1, self.lc == -1):
+        if np.logical_or(self.l0 == -1, self.lc == -1):
             raise errors.LengthError('Length cannot be less than 0.')
+        elif self.v == -1:
+            raise errors.VelocityError('Velocity of the object cannot be less than 0.')
         elif self.c == -1:
             raise errors.UnitError('Unit not passed correctly, please use \'m/s\' or \'rel\'')
 
     def __length_contraction(self):
         return self.l0 * self.beta()
 
+    def beta(self) -> float:
+        return np.sqrt(1 - (self.v / self.c)**2)
+
     def length_contraction(self, length_of_the_object: float, velocity_of_the_object: float) -> float:
         self.l0, self.v = length_of_the_object, velocity_of_the_object
 
         self.__check()
 
-        return self.l0 * self.beta()
+        return self.__length_contraction()
 
     def solver(self, original_length_of_the_object: Optional[float], measured_length_of_the_object: Optional[float], velocity_of_the_object: Optional[float]) -> float:
         self.v = velocity_of_the_object
         self.l0 = original_length_of_the_object
         self.lc = measured_length_of_the_object
+
         self.__check()
 
         return [self.__length_contraction() if self.lc is None else
                 self.lc / self.beta() if self.l0 is None else
-                self.c * np.sqrt(1 - self.lc**2 / self.l0**2) if np.logical_and(self.v is None, self.unit == 'm/s') else np.sqrt(1 - self.lc**2 / self.l0**2)][0]
+                self.c * np.sqrt(1 - (self.lc / self.l0)**2) if np.logical_and(self.v is None, self.unit == 'm/s') else np.sqrt(1 - (self.lc / self.l0)**2)][0]
--- 0004__relativistic_effects/relativistic_mass.py
+++ 0004__relativistic_effects/relativistic_mass.py
@@ -2,30 +2,48 @@
 Created on Mon Dec  6 00:00:08 2021
 """
 
+from typing import Optional
+
 import numpy as np
 
-import errors
+from utilities import errors
 
 
 class RelativisticMass:
 
-    def __init__(self, mass_of_the_object: float, velocity_of_the_object: float, unit: str = 'm/s'):
-        self.m0 = mass_of_the_object if mass_of_the_object > 0 else -1
-        self.v = velocity_of_the_object if velocity_of_the_object > 0 else -1
+    def __init__(self, unit: str = 'm/s'):
+        self.unit = unit
+        self.v, self.m0, self.mc = None, None, None
         self.c = 299792458 if unit == 'm/s' else 1 if unit == 'rel' else -1
 
     def __check(self):
-        if self.m0 == -1:
-            raise errors.LengthError('Length cannot be less than 0.')
+        if np.logical_or(self.m0 == -1, self.mc == -1):
+            raise errors.MassError('Mass cannot be less than 0.')
         elif self.v == -1:
             raise errors.VelocityError('Velocity of the object cannot be less than 0.')
         elif self.c == -1:
             raise errors.UnitError('Unit not passed correctly, please use \'m/s\' or \'rel\'')
 
+    def __relativistic_mass(self):
+        return self.m0 / self.beta()
+
     def beta(self) -> float:
         return np.sqrt(1 - (self.v / self.c)**2)
 
-    def relativistic_mass(self) -> float:
+    def relativistic_mass(self, mass_of_the_object: Optional[float], velocity_of_the_object: Optional[float]) -> float:
+        self.m0, self.v = mass_of_the_object, velocity_of_the_object
+
         self.__check()
 
-        return self.m0 / self.beta()
+        return self.__relativistic_mass()
+
+    def solver(self, mass_of_the_object: Optional[float], relativistic_mass: Optional[float], velocity_of_the_object: Optional[float]) -> float:
+        self.v = velocity_of_the_object
+        self.m0 = mass_of_the_object
+        self.mc = relativistic_mass
+
+        self.__check()
+
+        return [self.__relativistic_mass() if self.mc is None else
+                self.mc * self.beta() if self.m0 is None else
+                self.c * np.sqrt(1 - (self.m0 / self.mc)**2) if np.logical_and(self.v is None, self.unit == 'm/s') else np.sqrt(1 - (self.m0 / self.mc)**2)][0]
