5eb5a96ae67df7c93a26967e5a40e9ab20beb802
Noonuu <astrophysicsandpython@gmail.com>
2022-03-11 04:29:47 +0500

    ra-dec to gal-lat-long # 2
    * added RA/Dec constraints,
    * added DD to HHMMSS and DD to DDMMSS converters
    * removed style.css
    * some more minor stuff

--- 0006__coordinate_converters/back_end.js
+++ 0006__coordinate_converters/back_end.js
@@ -25,3 +25,75 @@ function eq_to_gal(){
     document.getElementById("gal-lat1").value = Math.degrees(Math.asin(f1+f2));
     document.getElementById("gal-long1").value = Math.degrees(f7);
 }
+
+function dd_to_hms(){
+    const message = document.getElementById("show-ra-err");
+    message.innerHTML = "";
+
+    ra = document.getElementById("ra1").value/15;
+
+    if (0 <= ra && ra <= 24){
+        _hh = Math.trunc(ra).toString();
+        _ra = ra - _hh;
+        _mm = Math.trunc(_ra*60).toString()
+        _ra = _ra*60 - _mm
+        _ss = Math.round(_ra * 60, 4).toString();
+
+        if (_hh < 10){
+            _hh = "0"+_hh
+        }
+        if (_mm < 10){
+            _mm = "0"+_mm
+        }
+        if (_ss < 10){
+            _ss = "0"+_ss
+        }
+
+        document.getElementById("ra-hms1").value = _hh + ":" + _mm + ":" + _ss;
+    } else {
+        document.getElementById("ra-hms1").value = "Err; RA ∈ [0, 360]";
+    }
+}
+
+function dd_to_dms(){
+    const message = document.getElementById("show-dec-err");
+    message.innerHTML = "";
+
+    dec = document.getElementById("dec1").value;
+
+    if (-90 <= dec && dec <= 90){
+        _dd = Math.trunc(dec).toString();
+
+        _dec = dec - _dd;
+
+        if (dec < 0) {
+            _dec = Math.abs(_dec);
+        }
+
+        _mm = Math.trunc(_dec*60).toString()
+        _dec = _dec*60 - _mm
+        _ss = Math.round(_dec * 60, 4).toString();
+
+        if (0 < _dd && _dd < 10){
+            _dd = "0"+_dd;
+        }
+        if (-10 < _dd && _dd < 0){
+            _dd = "-0"+Math.abs(_dd)
+        }
+
+        if (_mm < 10){
+            _mm = "0"+_mm;
+        }
+        if (_ss < 10){
+            _ss = "0" + _ss;
+        }
+        if (_ss == 60){
+            _ss = "00";
+            _mm = parseFloat(_mm) + 1
+        }
+
+        document.getElementById("dec-dms1").value = _dd + ":" + _mm + ":" + _ss;
+    } else {
+        document.getElementById("dec-dms1").value = "Err; Dec ∈ [-90, +90]";
+    }
+}
