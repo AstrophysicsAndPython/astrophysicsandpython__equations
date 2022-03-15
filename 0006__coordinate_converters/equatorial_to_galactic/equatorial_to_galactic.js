c61a1f165adff7f57048e23f2c38b4d15864fa65
noonu <astrophysicsandpython@gmail.com>
2022-03-15 09:49:21 +0500

    ra-dec to gal-lat-long # 5
    * syntactical corrections to equatorial_to_galactic.js
    * minor corrections to equatorial_to_galactic.html

--- 0006__coordinate_converters/equatorial_to_galactic/equatorial_to_galactic.js
+++ 0006__coordinate_converters/equatorial_to_galactic/equatorial_to_galactic.js
@@ -1,16 +1,16 @@
-Math.radians = function(degrees) {
-	return degrees * Math.PI / 180;
-}
+Math.radians = function (degrees) {
+    return degrees * Math.PI / 180;
+};
 
-Math.degrees = function(radians) {
-    return radians * 180/Math.PI;
-}
+Math.degrees = function (radians) {
+    return radians * 180 / Math.PI;
+};
 
-l_ncp = Math.radians(122.9320)
-dec_ngp = Math.radians(27.1284)
-ra_ngp = Math.radians(192.8595)
+l_ncp = Math.radians(122.9320);
+dec_ngp = Math.radians(27.1284);
+ra_ngp = Math.radians(192.8595);
 
-function eq_to_gal(){
+function eq_to_gal() {
     ra = document.getElementById("ra1").value;
     dec = document.getElementById("dec1").value;
 
@@ -33,52 +33,35 @@ function eq_to_gal(){
     }
 }
 
-function dd_to_hms(box_number){
-    if (box_number == 1){
-        ra = parseFloat(document.getElementById("ra1").value)/15;
-    } else {
-        ra = parseFloat(document.getElementById("ra2").value)/15;
-    }
+function dd_to_hms() {
+    ra = parseFloat(document.getElementById("ra1").value) / 15;
 
-    if (0 <= ra && ra <= 24){
+    if (0 <= ra && ra <= 24) {
         _hh = Math.trunc(ra).toString();
-        _ra = Math.round((ra - _hh)*1e6)/1e6;
-        _mm = Math.trunc(_ra * 60).toString()
-        _ra = _ra*60 - _mm
+        _ra = Math.round((ra - _hh) * 1e6) / 1e6;
+        _mm = Math.trunc(_ra * 60).toString();
+        _ra = _ra * 60 - _mm;
         _ss = Math.round(_ra * 60, 4).toString();
 
-        if (_hh < 10){
-            _hh = "0"+_hh
+        if (_hh < 10) {
+            _hh = "0" + _hh;
         }
-        if (_mm < 10){
-            _mm = "0"+_mm
+        if (_mm < 10) {
+            _mm = "0" + _mm;
         }
-        if (_ss < 10){
-            _ss = "0"+_ss
+        if (_ss < 10) {
+            _ss = "0" + _ss;
         }
-
-        if (box_number == 1){
-            document.getElementById("ra-hms1").value = _hh + ":" + _mm + ":" + _ss;
-        } else {
-            document.getElementById("ra-hms2").value = _hh + ":" + _mm + ":" + _ss;
-        }
+        document.getElementById("ra-hms1").value = _hh + ":" + _mm + ":" + _ss;
     } else {
-        if (box_number == 1){
-            document.getElementById("ra-hms1").value = "Err; RA ∈ [0, 360]";
-        } else {
-            document.getElementById("ra-hms2").value = "Err; RA ∈ [0, 360]";
-        }
+        document.getElementById("ra-hms1").value = "Err; RA ∈ [0, 360]";
     }
 }
 
-function dd_to_dms(box_number){
-    if (box_number == 1){
-        dec = document.getElementById("dec1").value;
-    } else {
-        dec = document.getElementById("dec2").value;
-    }
+function dd_to_dms() {
+    dec = document.getElementById("dec1").value;
 
-    if (-90 <= dec && dec <= 90){
+    if (-90 <= dec && dec <= 90) {
         _dd = Math.trunc(dec).toString();
 
         _dec = dec - _dd;
@@ -87,38 +70,30 @@ function dd_to_dms(box_number){
             _dec = Math.abs(_dec);
         }
 
-        _mm = Math.trunc(_dec*60).toString()
-        _dec = _dec*60 - _mm
+        _mm = Math.trunc(_dec * 60).toString();
+        _dec = _dec * 60 - _mm;
         _ss = Math.round(_dec * 60, 4).toString();
 
-        if (0 < _dd && _dd < 10){
-            _dd = "0"+_dd;
+        if (0 < _dd && _dd < 10) {
+            _dd = "0" + _dd;
         }
-        if (-10 < _dd && _dd < 0){
-            _dd = "-0"+Math.abs(_dd)
+        if (-10 < _dd && _dd < 0) {
+            _dd = "-0" + Math.abs(_dd);
         }
 
-        if (_mm < 10){
-            _mm = "0"+_mm;
+        if (_mm < 10) {
+            _mm = "0" + _mm;
         }
-        if (_ss < 10){
+        if (_ss < 10) {
             _ss = "0" + _ss;
         }
-        if (_ss == 60){
+        if (_ss === 60) {
             _ss = "00";
-            _mm = parseFloat(_mm) + 1
+            _mm = parseFloat(_mm) + 1;
         }
 
-        if (box_number == 1){
-            document.getElementById("dec-dms1").value = _dd + ":" + _mm + ":" + _ss;
-        } else {
-            document.getElementById("dec-dms2").value = _dd + ":" + _mm + ":" + _ss;
-        }
+        document.getElementById("dec-dms1").value = _dd + ":" + _mm + ":" + _ss;
     } else {
-        if (box_number == 1){
-            document.getElementById("dec-dms1").value = "Err; Dec ∈ [-90, +90]";
-        } else {
-            document.getElementById("dec-dms2").value = "Err; Dec ∈ [-90, +90]";
-        }
+        document.getElementById("dec-dms1").value = "Err; Dec ∈ [-90, +90]";
     }
 }
