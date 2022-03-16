402f8fc2a19fdba0f97012bbec414c1bf41764b5
Noonuu <astrophysicsandpython@gmail.com>
2022-03-17 00:33:02 +0500

    ra-dec to gal-lat-long # 9
    * finalized the ra/dec to gal-lat/long calculators

--- 0006__coordinate_converters/galactic_to_equatorial/GalToEq.js
+++ 0006__coordinate_converters/galactic_to_equatorial/GalToEq.js
@@ -39,3 +39,72 @@ function to_equatorial()
     }
 
 }
+
+function to_hms()
+{
+    ra = parseFloat(document.getElementById("ra1").value)*15;
+
+    if (0 <= ra && ra <= 360){
+        _hh = Math.trunc(ra).toString();
+        _ra = Math.round((ra - _hh)*1e6)/1e6;
+        _mm = Math.trunc(_ra * 60).toString()
+        _ra = _ra*60 - _mm
+        _ss = (_ra * 60).toFixed(4).toString();
+
+        if (_hh < 10){
+            _hh = "0"+_hh
+        }
+        if (_mm < 10){
+            _mm = "0"+_mm
+        }
+        if (_ss < 10){
+            _ss = "0"+_ss
+        }
+
+        document.getElementById("ra-hms").value = _hh + ":" + _mm + ":" + _ss;
+    } else {
+        document.getElementById("ra-hms").value = "Err; RA ∈ [0, 360]";
+    }
+}
+
+function to_dms()
+{
+    dec = document.getElementById("dec1").value;
+
+    if (-90 <= dec && dec <= 90){
+        _dd = Math.trunc(dec).toString();
+
+        _dec = dec - _dd;
+
+        if (dec < 0) {
+            _dec = Math.abs(_dec);
+        }
+
+        _mm = Math.trunc(_dec*60).toString()
+        _dec = _dec*60 - _mm
+        _ss = (_dec * 60).toFixed(4).toString();
+
+        if (0 < _dd && _dd < 10){
+            _dd = "0"+_dd;
+        }
+        if (-10 < _dd && _dd < 0){
+            _dd = "-0"+Math.abs(_dd)
+        }
+
+        if (_mm < 10){
+            _mm = "0"+_mm;
+        }
+        if (_ss < 10){
+            _ss = "0" + _ss;
+        }
+        if (_ss == 60){
+            _ss = "00";
+            _mm = parseFloat(_mm) + 1
+        }
+
+        document.getElementById("dec-dms").value = _dd + ":" + _mm + ":" + _ss;
+    } else
+    {
+        document.getElementById("dec-dms").value = "Err; Dec ∈ [-90, +90]";
+    }
+}
